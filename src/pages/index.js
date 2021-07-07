d051973641eec28880438ddb27dcaa682cc97db5
mabvmex <mabvmex@gmail.com>
2021-07-06 22:16:24 -0500

    Añadiendo Semantic UI React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Button } from "semantic-ui-react"
 
 export default function Home() {
   return (
     <div>
       <h1> ::: HOME ::: </h1>
       <p> ::: esto es un parrafo ::: </p>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   )
 }
