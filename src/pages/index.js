26e337364ab8fefd3da07cbcf0fac3d5ee555433
mabvmex <mabvmex@gmail.com>
2021-07-09 14:39:40 -0500

    obteniendo post - consulta graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,31 @@
 import React from "react"
 import { Button } from "semantic-ui-react"
+import { useStaticQuery, graphql } from "gatsby"
 import BlogLayout from "../layouts/BlogLayout"
 
 export default function Home() {
+  const result = useStaticQuery(graphql`
+    query {
+      allStrapiPost {
+        nodes {
+          id
+          title
+          content
+          createdAt
+          miniatura {
+            formats {
+              thumbnail {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(result)
+
   return (
     <BlogLayout>
       <h1> Esto es la HOME del Blog </h1>
